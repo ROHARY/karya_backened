e32b81bedb9360fc5be88ef8affc295d73c67e35
Aryan Gupta <aryanz7027@gmail.com>
2025-03-16 00:14:47 +0530

    fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const DB_NAME = process.env.DB_NAME || 'todos'
-const MONGO_URL = (process.env.MONGO_URL) || `mongodb://127.0.0.1:27017/`
+const MONGO_URL = (process.env.MONGO_URI) || `mongodb://127.0.0.1:27017/`
 
 exports.connect = async function connect(){
+  console.log('MONGO_URL', MONGO_URL)
   try{
     return await mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, dbName: DB_NAME  })
   }catch(err){
